892a4f4b8f44c35f9edf6442b7bf90f903690a01
user <user@pdl-905904e3f76.bnf.brain.riken.jp>
2010-07-16 22:50:07 +0900

    same example code with the comments stripped

---
+++ example_simple_distilled.py
@@ -0,0 +1,40 @@
+
+# >>> import and initialization >>> 
+
+import egi.simple as egi
+## import egi.threaded as egi
+
+# ms_localtime = egi.egi_internal.ms_localtime     
+ms_localtime = egi.ms_localtime     
+
+
+ns = egi.Netstation()
+ns.connect('11.0.0.42', 55513) # sample address and port -- change according to your network settings
+## ns.initialize('11.0.0.42', 55513)
+ns.BeginSession()     
+
+ns.sync()     
+
+ns.StartRecording()
+
+
+
+# >>> send many events here >>> 
+
+## # optionally can perform additional synchronization     
+## ns.sync()     
+ns.send_event( 'evt_', label="event", timestamp=egi.ms_localtime(), table = {'fld1' : 123, 'fld2' : "abc", 'fld3' : 0.042} ) 
+
+
+
+# >>> we have sent all we wanted, time to go home >>> 
+
+ns.StopRecording()
+
+ns.EndSession()     
+ns.disconnect()
+
+## ns.EndSession()     
+## ns.finalize()     
+
+# >>> that's it !
