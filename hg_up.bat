0af24f3557f3504c28efc88ec5512886e418d41d
user <user@pdl-905904e3f76.bnf.brain.riken.jp>
2010-07-17 23:28:33 +0900

    complifications for the uploader )

--- hg_up.bat
+++ hg_up.bat
@@ -1,1 +1,5 @@
+hg ci
+hg pull https://pynetstation.googlecode.com/hg/   
+hg merge
+hg ci -m "pulled from https://pynetstation.googlecode.com/hg/ and merged"
 hg push https://pynetstation.googlecode.com/hg/   
