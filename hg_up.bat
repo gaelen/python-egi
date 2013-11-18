31cdc1c5b2d1e79c7e8a97d59f9ff435e2c2baeb
Gaelen Hadlett <gaelenh@gmail.com>
2013-11-17 20:46:33 -0500

    Getting ready for pypi packaging

--- hg_up.bat
+++
@@ -1,5 +0,0 @@
-hg ci
-hg pull https://pynetstation.googlecode.com/hg/   
-hg merge
-hg ci -m "pulled from https://pynetstation.googlecode.com/hg/ and merged"
-hg push https://pynetstation.googlecode.com/hg/   
---
+++ setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# standard
+from __future__ import absolute_import
+from distutils.core import setup
+import os
+
+# local
+from egi import __version__
+
+
+def main():
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(cwd, 'README.txt')
+    readme = open(path, 'rb').read()
+
+    setup(
+        name='egi',
+        description='',
+        long_description=readme,
+        version=__version__,
+        #license='Apache 2.0',
+        author='Gaelen Hadlett',
+        author_email='gaelenh@gmail.com',
+        url='https://github.com/gaelenh/python-egi',
+        py_modules=['egi'],
+        keywords=['egi', 'netstation', 'pynetstation'],
+        classifiers=[
+            'License :: OSI Approved :: Apache Software License',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: POSIX :: Linux',
+            'Operating System :: Unix'
+        ]
+    )
+
+if __name__ == '__main__':
+    main()
